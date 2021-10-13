48d293bc9f5579f203eb14a3386a7a07f5d0c8f2
Christian <33060837+tianpaul01@users.noreply.github.com>
2021-10-13 22:18:49 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip3 install Pillow image;
 pip3 install --upgrade tensorflow keras numpy pandas sklearn pillow;
-python3 animerecommend.py;
+python3 ./anime_recommend/animerecommend.py;
 rm regression_model.h5;
